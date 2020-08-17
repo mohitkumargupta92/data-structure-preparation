21fc60d0f36bb99d2e5b6f6a99b33c6ce2771165
GUPTA Mohit <mohgupta@one.steria.dom>
2020-08-17 09:31:25 +0530

    Basic Operation of Binary Search Tree

--- src/com/datastructure/tree/binarysearchtree/BinarySearchTree.java
+++ src/com/datastructure/tree/binarysearchtree/BinarySearchTree.java
@@ -1,9 +1,8 @@
 package com.datastructure.tree.binarysearchtree;
 
-import javax.swing.text.html.HTMLDocument.HTMLReader.ParagraphAction;
-
 public class BinarySearchTree {
 
+	static final int COUNT = 1;
 	private Node root;
 
 	public boolean isEmpty() {
@@ -38,6 +37,25 @@ public class BinarySearchTree {
 
 	}
 
+	public boolean find(int data) {
+		if (isEmpty()) {
+			return false;
+		}
+
+		Node node = root;
+		while (node != null) {
+			if (data == node.getData()) {
+				return true;
+			} else if (data < node.getData()) {
+				node = node.getLeftChild();
+			} else {
+				node = node.getRightChild();
+			}
+		}
+
+		return false;
+	}
+
 	public int minimumValue() {
 		Node parentNode = root;
 		Node currentNode = root;
@@ -50,6 +68,26 @@ public class BinarySearchTree {
 		return parentNode.getData();
 	}
 
+	public Node getSuccessor(Node node) {
+		if (null == node) {
+			return null;
+		}
+
+		Node currentNode = node;
+		Node parentNode = node;
+
+		while (currentNode != null) {
+			if (currentNode.getLeftChild() == null) {
+				parentNode.setLeftChild(null);
+				break;
+			}
+			parentNode = currentNode;
+			currentNode = currentNode.getLeftChild();
+		}
+
+		return currentNode;
+	}
+
 	public int maximumValue() {
 		Node parentNode = root;
 		Node currentNode = root;
@@ -60,5 +98,134 @@ public class BinarySearchTree {
 
 		}
 		return parentNode.getData();
+	}
+
+	public boolean delete(int data) {
+		if (isEmpty()) {
+			return false;
+		}
+
+		Node parentNode = root;
+		Node currentNode = root;
+		boolean isLeftChild = false;
+
+		while (currentNode.getData() != data) {
+			parentNode = currentNode;
+			if (data < currentNode.getData()) {
+				currentNode = currentNode.getLeftChild();
+				isLeftChild = true;
+			} else {
+				currentNode = currentNode.getRightChild();
+				isLeftChild = false;
+			}
+
+			if (null == currentNode) {
+				return false;
+			}
+		}
+
+		if (null == currentNode.getLeftChild() && null == currentNode.getRightChild()) {
+			// currentNode has no Child
+			if (currentNode == root) {
+				root = null;
+			} else if (isLeftChild) {
+				parentNode.setLeftChild(null);
+			} else {
+				parentNode.setRightChild(null);
+			}
+		} else if (null == currentNode.getRightChild()) {
+			// currentNode has only Left Child
+			if (currentNode == root) {
+				root = root.getLeftChild();
+			}
+			if (isLeftChild) {
+				parentNode.setLeftChild(currentNode.getLeftChild());
+			} else {
+				parentNode.setRightChild(currentNode.getLeftChild());
+			}
+		} else if (null == currentNode.getLeftChild()) {
+			// currentNode has only Right Child
+			if (currentNode == root) {
+				root = root.getRightChild();
+			} else if (isLeftChild) {
+				parentNode.setLeftChild(currentNode.getRightChild());
+			} else {
+				parentNode.setRightChild(currentNode.getRightChild());
+			}
+		} else {
+			// currentNode has both Children
+			Node successorNode = getSuccessor(currentNode.getRightChild());
+			if (currentNode == root) {
+				root = successorNode;
+			} else if (isLeftChild) {
+				parentNode.setLeftChild(successorNode);
+			} else {
+				parentNode.setRightChild(successorNode);
+			}
+			successorNode.setLeftChild(currentNode.getLeftChild());
+			successorNode.setRightChild(currentNode.getRightChild());
+		}
+
+		return true;
+	}
+
+	public void print2DUtil(Node root, int space) {
+		// Base case
+		if (root == null)
+			return;
+
+		// Increase distance between levels
+		space += COUNT;
+
+		// Process right child first
+		print2DUtil(root.getRightChild(), space);
+
+		// Print current node after space
+		// count
+		System.out.print("\n");
+		for (int i = COUNT; i < space; i++)
+			System.out.print(" ");
+		System.out.print(root.getData() + "\n");
+
+		// Process left child
+		print2DUtil(root.getLeftChild(), space);
+	}
+
+	boolean isBST() {
+		return isBSTUtil(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+	/*
+	 * Returns true if the given tree is a BST and its values are >= min and <= max.
+	 */
+	boolean isBSTUtil(Node node, int min, int max) {
+		/* an empty tree is BST */
+		if (node == null)
+			return true;
+
+		/* false if this node violates the min/max constraints */
+		if (node.getData() < min || node.getData() > max)
+			return false;
+
+		/*
+		 * otherwise check the subtrees recursively tightening the min/max constraints
+		 */
+		// Allow only distinct values
+		return (isBSTUtil(node.getLeftChild(), min, node.getData() - 1)
+				&& isBSTUtil(node.getRightChild(), node.getData() + 1, max));
+	}
+
+	// Wrapper over print2DUtil()
+	public void print2D(Node root) {
+		// Pass initial space count as 0
+		print2DUtil(root, 0);
+	}
+
+	public Node getRoot() {
+		return root;
+	}
+
+	public void setRoot(Node root) {
+		this.root = root;
 	}
 }
--- src/com/datastructure/tree/binarysearchtree/TestBinarySearchTree.java
+++ src/com/datastructure/tree/binarysearchtree/TestBinarySearchTree.java
@@ -5,12 +5,24 @@ public class TestBinarySearchTree {
 	public static void main(String[] args) {
 		BinarySearchTree tree = new BinarySearchTree();
 
-		for (int i = 0; i < 5; i++) {
+		for (int i = 10; i > 0; i--) {
+			tree.insert(i);
+		}
+
+		for (int i = 20; i > 10; i--) {
 			tree.insert(i);
 		}
 		
-		System.out.println(tree.minimumValue());
-		System.out.println(tree.maximumValue());
+		tree.insert(100);
+		tree.delete(10);
+		tree.delete(15);
+		tree.print2D(tree.getRoot());
+		System.out.println(tree.isBST());
+		
+
+//		System.out.println(tree.minimumValue());
+//		System.out.println(tree.maximumValue());
+//		System.out.println(tree.find(6));
 
 	}
 
